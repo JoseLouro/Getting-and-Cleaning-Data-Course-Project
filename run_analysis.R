1e0da6ec0177c0b10b3998360d00c9ca3800fe68
José Louro <JoseLouro@users.noreply.github.com>
2015-02-22 19:55:51 +0000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -76,4 +76,4 @@ names(Data)<-gsub("BodyBody", "Body", names(Data))
 library(plyr);
 Data2 <- aggregate(. ~subject + activity, Data, mean)
 Data2 <- Data2[order(Data2$subject,Data2$activity),]
-write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+write.table(Data2, file = "tidyData.txt",row.name=FALSE)
