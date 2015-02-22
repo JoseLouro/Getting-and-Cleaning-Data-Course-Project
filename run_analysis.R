983536decc8145af4c5994197e17c594436e1365
Jose Louro <JoseLouro@gmail.com>
2015-02-22 19:02:41 +0000

    Signed-off-by: Jose Louro <JoseLouro@gmail.com>

--- run_analysis.R
+++ run_analysis.R
@@ -9,7 +9,6 @@
 ### https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 ###
 ### All the data is stored in a folder named 'data'
-###
 ### 1. Merge the training and the test sets to create one data set.
 ### 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 ### 3. Use descriptive activity names to name the activities in the data set
